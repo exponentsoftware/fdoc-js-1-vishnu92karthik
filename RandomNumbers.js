99eb41f7853efbc77c51d5f07e5aa2fd51f75858
Vishnu Karthik Madupuri <72738581+vishnu92karthik@users.noreply.github.com>
2021-09-18 16:38:29 +0530

    Add files via upload

---
+++ RandomNumbers.js
@@ -0,0 +1,10 @@
+const random7Number = [];
+
+for(let i =0;i<7; i++){
+function sevenRandomNumbers(){
+ const randomNumber = Math.floor(Math.random() *20);
+ random7Number.push(randomNumber)
+ }
+ sevenRandomNumbers()
+ }
+ console.log(random7Number);
